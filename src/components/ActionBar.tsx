20e59eed3a9f644e5934b0fccba60d8ebd92b0cf
Den <iamdennshi@gmail.com>
2023-09-16 00:43:16 +0500

    Add Active Button in ActiveBar

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,12 +1,24 @@
 import AddButton from "./AddButton";
 
-function HomeButton() {
+function HomeButton({ isActive }: { isActive?: boolean }) {
+  const color = isActive ? "#fff" : "#649fa9";
+
   return (
-    <div className="cursor-pointer active:scale-90 transition">
-      <svg viewBox="0 0 1024 1024" fill="#fff" height="2.5em" width="2.5em">
+    <button
+      className={`block p-2 cursor-pointer  rounded-full   ${
+        isActive && "bg-green-500 button-shadow--active"
+      }`}
+    >
+      <svg
+        className="active:scale-90 transition"
+        viewBox="0 0 1024 1024"
+        fill={`${color}`}
+        height="2em"
+        width="2em"
+      >
         <path d="M946.5 505L534.6 93.4a31.93 31.93 0 00-45.2 0L77.5 505c-12 12-18.8 28.3-18.8 45.3 0 35.3 28.7 64 64 64h43.4V908c0 17.7 14.3 32 32 32H448V716h112v224h265.9c17.7 0 32-14.3 32-32V614.3h43.4c17 0 33.3-6.7 45.3-18.8 24.9-25 24.9-65.5-.1-90.5z" />
       </svg>
-    </div>
+    </button>
   );
 }
 
@@ -17,9 +29,9 @@ export default function ActionBar(props: ActionBarProps) {
         props.hideActionBar ? "-bottom-24" : "bottom-8"
       }`}
     >
-      <ul className="flex justify-evenly items-center w-full h-full -mr-[3px] bg-green-500 rounded-l-full max-w-xs">
+      <ul className="flex justify-evenly items-center w-full h-full -mr-[3px] bg-[#236e78] rounded-l-full max-w-xs">
         <li className="">
-          <HomeButton />
+          <HomeButton isActive />
         </li>{" "}
         <li className="">
           <HomeButton />
@@ -37,12 +49,12 @@ export default function ActionBar(props: ActionBarProps) {
             fill-rule="evenodd"
             clip-rule="evenodd"
             d="M3 0H0V69H87V0H84C84 22.0914 65.8676 40 43.5 40C21.1324 40 3 22.0914 3 0Z"
-            fill="#22c55e"
+            fill="#236e78"
           />
         </svg>
         <AddButton />
       </div>
-      <ul className="flex justify-evenly items-center w-full h-full -ml-[3px] bg-green-500 rounded-r-full max-w-xs">
+      <ul className="flex justify-evenly items-center w-full h-full -ml-[3px] bg-[#236e78] rounded-r-full max-w-xs">
         <li className="">
           <HomeButton />
         </li>{" "}

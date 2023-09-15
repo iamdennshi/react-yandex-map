dd5bf91d67c284be6d0db76b03c1f85eddbf6d2c
Den <iamdennshi@gmail.com>
2023-09-16 01:31:21 +0500

    Switching in ActionBar

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,47 +1,74 @@
+import { useState } from "react";
 import AddButton from "./AddButton";
 
-function HomeButton({ isActive }: { isActive?: boolean }) {
-  const color = isActive ? "#fff" : "#649fa9";
+interface HomeButtonProps {
+  id: number;
+  isActive: boolean;
+  onClick: Function;
+}
+
+function HomeButton(props: HomeButtonProps) {
+  const color = props.isActive ? "#fff" : "#649fa9";
 
   return (
     <button
-      className={`block p-2 cursor-pointer  rounded-full   ${
-        isActive && "bg-green-500 button-shadow--active"
-      }`}
+      onClick={() => props.onClick(props.id)}
+      className={`relative block p-2 cursor-pointer z-20`}
     >
-      <svg
-        className="active:scale-90 transition"
-        viewBox="0 0 1024 1024"
-        fill={`${color}`}
-        height="2em"
-        width="2em"
-      >
-        <path d="M946.5 505L534.6 93.4a31.93 31.93 0 00-45.2 0L77.5 505c-12 12-18.8 28.3-18.8 45.3 0 35.3 28.7 64 64 64h43.4V908c0 17.7 14.3 32 32 32H448V716h112v224h265.9c17.7 0 32-14.3 32-32V614.3h43.4c17 0 33.3-6.7 45.3-18.8 24.9-25 24.9-65.5-.1-90.5z" />
-      </svg>
+      <div
+        className={`absolute -z-10 inset-0 transition duration-500 w-full h-full rounded-full ${
+          props.isActive ? "bg-green-500 button-shadow--active sca" : "scale-0"
+        } `}
+      ></div>
+      <div className="active:scale-90 transition">
+        <svg
+          className="transition duration-500"
+          viewBox="0 0 1024 1024"
+          fill={`${color}`}
+          height="2em"
+          width="2em"
+        >
+          <path d="M946.5 505L534.6 93.4a31.93 31.93 0 00-45.2 0L77.5 505c-12 12-18.8 28.3-18.8 45.3 0 35.3 28.7 64 64 64h43.4V908c0 17.7 14.3 32 32 32H448V716h112v224h265.9c17.7 0 32-14.3 32-32V614.3h43.4c17 0 33.3-6.7 45.3-18.8 24.9-25 24.9-65.5-.1-90.5z" />
+        </svg>
+      </div>
     </button>
   );
 }
 
 export default function ActionBar(props: ActionBarProps) {
+  const [active, setActive] = useState(0);
+
+  const onClickButton = (id: number) => {
+    setActive(id);
+  };
+
   return (
     <div
-      className={`absolute max-w-md h-16 w-full px-4 left-1/2 -translate-x-1/2 flex transition-all duration-500 ${
+      className={`absolute max-w-sm h-[66px] w-full px-4 left-1/2 -translate-x-1/2 flex transition-all duration-500 ${
         props.hideActionBar ? "-bottom-24" : "bottom-8"
       }`}
     >
-      <ul className="flex justify-evenly items-center w-full h-full -mr-[3px] bg-[#236e78] rounded-l-full max-w-xs">
+      <ul className="flex justify-around items-center w-full h-full -mr-[3px] bg-[#236e78] rounded-l-full max-w-xs">
         <li className="">
-          <HomeButton isActive />
-        </li>{" "}
+          <HomeButton
+            id={0}
+            isActive={active === 0 ? true : false}
+            onClick={onClickButton}
+          />
+        </li>
         <li className="">
-          <HomeButton />
+          <HomeButton
+            id={1}
+            isActive={active === 1 ? true : false}
+            onClick={onClickButton}
+          />
         </li>
       </ul>
       <div className="relative bottom-[5px]">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="87"
-          height="69"
+          height="71"
           viewBox="0 0 87 55"
           fill="none"
         >
@@ -56,10 +83,18 @@ export default function ActionBar(props: ActionBarProps) {
       </div>
       <ul className="flex justify-evenly items-center w-full h-full -ml-[3px] bg-[#236e78] rounded-r-full max-w-xs">
         <li className="">
-          <HomeButton />
-        </li>{" "}
+          <HomeButton
+            id={2}
+            isActive={active === 2 ? true : false}
+            onClick={onClickButton}
+          />
+        </li>
         <li className="">
-          <HomeButton />
+          <HomeButton
+            id={3}
+            isActive={active === 3 ? true : false}
+            onClick={onClickButton}
+          />
         </li>
       </ul>
     </div>

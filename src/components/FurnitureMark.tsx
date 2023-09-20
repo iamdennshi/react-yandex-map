4932af5c4e4ba1166be656aac2f50ee7e76e05b9
Den <iamdennshi@gmail.com>
2023-09-20 07:52:22 +0500

    Update items card

--- src/components/FurnitureMark.tsx
+++ src/components/FurnitureMark.tsx
@@ -6,23 +6,22 @@ export function FurnitureMark(props: FurnitureProps) {
 
   const stringify = JSON.stringify(props.info).replaceAll('"', "'");
   const handleEdit = `window.editMark(${props.placeID}, ${stringify}, 'furniture')`;
-  const content = `<div class="md:w-[500px] w-[300px] my-balloon flex flex-col md:flex-row">
-  <div class="basis-1/2 cursor-pointer md:mr-4 mt-4 md:mt-0">
-    <img class="h-full w-full object-cover rounded-2xl " src="${
-      props.info.img
-    }"/>
+  const content = `
+  <div class="max-h-[600px] w-[300px]">
+  <div class="cursor-pointer">
+    <img class="h-[260px] w-full object-cover" src="${props.info.img}"/>
   </div>
-  <div class="basis-1/2 flex flex-col">
-    <h2 class="text-center text-2xl font-bold text-[#4A5568] md:mb-2 my-2 ">${props.info.name.toLocaleUpperCase()}</h2>
+  <div class="">
+    <h2 class="text-center text-2xl font-bold text-[#4A5568]  my-2 ">${props.info.name.toLocaleUpperCase()}</h2>
     <h3 class="w-[61px] mx-auto text-center rounded-md bg-[#FFEEDD] text-[#D39658]">МАФ</h3>
-    <ul class="flex flex-col my-4 gap-2 max-h-44 overflow-y-scroll ">
+    <ul class="flex flex-col px-4 my-4 gap-2 max-h-44 overflow-y-scroll ">
       <li class="text-[#4A5568]">Состояние: <span class="font-bold">удовлетворительное</span></li>
       <li class="text-[#4A5568]">Комментарий: <span class="font-bold break-words">${
         props.info.comment
       }</span>
     </ul>
     <button onclick="${handleEdit}" class="block px-4 py-1 m-auto border-solid border-[1px] text-[#D39658] border-[#D39658] rounded mb-2">Редактировать</button>
-    <div class="text-center text-[#B2ABAB]">Последнее изменение 02.07.2023 16:37</div>
+    <div class="text-center text-[#B2ABAB] mb-2"">Последнее изменение 02.07.2023 16:37</div>
   </div>
 </div>`;
 
--- src/components/TreeMark.tsx
+++ src/components/TreeMark.tsx
@@ -4,16 +4,15 @@ import React from "react";
 export function TreeMark(props: TreeProps) {
   console.log("TreeMark render");
 
-  const content = `<div class="md:w-[500px] w-[300px] my-balloon flex  flex-col md:flex-row">
-  <div class="basis-1/2 cursor-pointer md:mr-4 mt-4 md:mt-0">
-    <img class="h-full w-full object-cover rounded-2xl" src="${
-      props.info.img
-    }"/>
+  const content = `
+  <div class="w-[300px] max-h-[600px]">
+  <div class="cursor-pointer">
+    <img class="h-[260px] w-full object-cover" src="${props.info.img}"/>
   </div>
-  <div class="basis-1/2 flex flex-col">
-    <h2 class="text-center text-2xl font-bold text-[#4A5568] md:mb-2 my-2 ">${props.info.name.toLocaleUpperCase()}</h2>
+  <div>
+    <h2 class="text-center text-2xl font-bold text-[#4A5568]  my-2 ">${props.info.name.toLocaleUpperCase()}</h2>
     <h3 class="w-[61px] mx-auto text-center rounded-md bg-[#DDFFE0] text-[#58D364]">дерево</h3>
-    <ul class="flex flex-col my-4 gap-2 max-h-44 overflow-y-scroll ">
+    <ul class="flex flex-col px-4 my-4 gap-2 max-h-44 overflow-y-scroll ">
       <li class="text-[#4A5568]">Высота: <span class="font-bold">${
         props.info.height
       } м</span></li>
@@ -26,12 +25,12 @@ export function TreeMark(props: TreeProps) {
       <li class="text-[#4A5568]">Состояние: <span class="font-bold">удовлетворительное</span></li>
       <li class="text-[#4A5568]">Комментарий: <span class="font-bold break-words">${
         props.info.comment
-      }</span>
+      } </span>
     </ul>
     <button onclick="window.editMark(${
       props.info.id
     })" class="block px-4 py-1 m-auto border-solid border-[1px] text-[#58D364] border-[#58D364] rounded mb-2">Редактировать</button>
-    <div class="text-center text-[#B2ABAB]">Последнее изменение 02.07.2023 16:37</div>
+    <div class="text-center text-[#B2ABAB] mb-2">Последнее изменение 02.07.2023 16:37</div>
   </div>
 </div>`;
 

65172236d0e629724a247c720e8b8da252401f95
Den <iamdennshi@gmail.com>
2023-09-23 00:49:31 +0500

    Replace rotate with transform: rotate

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -128,50 +128,19 @@ function Info(props: { isActive: boolean, setActive: Function }) {
               <dd>12 345 кв.м</dd>
             </dl>
           </div>
-          <div className="bg-[#F2F6F6] rounded-[15px] px-[18px] py-[16px] mt-[12px] active:bg-[#CCDADC] transition-all">
+          <div className=" bg-[#F2F6F6] rounded-[15px] px-[18px] py-[16px] mt-[12px] active:bg-[#CCDADC] transition-all">
             <input className='accordion' id='one' type='checkbox'/>
             <label htmlFor='one' className=''>
               <div className='flex text-primary font-bold'>
                 <div className='pr-2 '>
-                  <svg className='accordion-arrow rotate-90 h-full transition-all' xmlns="http://www.w3.org/2000/svg" width="7" height="11" viewBox="0 0 7 11" fill="none">
+                  <svg className='accordion-arrow h-full transition-all' xmlns="http://www.w3.org/2000/svg" width="7" height="11" viewBox="0 0 7 11" fill="none">
                     <path d="M0 11L-4.80825e-07 0L7 5.5L0 11Z" fill="#024751"/>
                   </svg>
                 </div>
                 <p>Общая площадь</p>
                 <span className='block flex-1 text-right'>12 345 кв.м</span>
               </div>
-              <ul className='accordion-item border-t-2 border-[#E6EDEE] mt-2 pt-1'>
-                <li className='flex text-primary gap-1 mt-1'>
-                  <p className='pl-[15px]'>Деревья</p>
-                  <div className='basis-full h-[17px] border-b-[2px] border-[#ABC2C5] border-dotted'></div>
-                  <span className='block whitespace-nowrap'>50 шт</span>
-                </li>
-                <li className='flex text-primary gap-1 mt-1'>
-                  <p className='pl-[15px]'>Деревья</p>
-                  <div className='basis-full h-[17px] border-b-[2px] border-[#ABC2C5] border-dotted'></div>
-                  <span className='block whitespace-nowrap'>50 шт</span>
-                </li>
-                <li className='flex text-primary gap-1 mt-1'>
-                  <p className='pl-[15px]'>Деревья</p>
-                  <div className='basis-full h-[17px] border-b-[2px] border-[#ABC2C5] border-dotted'></div>
-                  <span className='block whitespace-nowrap'>50 шт</span>
-                </li>
-              </ul>
-            </label>
-          </div>
-          <div className="bg-[#F2F6F6] rounded-[15px] px-[18px] py-[16px] mt-[12px] active:bg-[#CCDADC] transition-all">
-            <input className='accordion' id='two' type='checkbox'/>
-            <label htmlFor='two' className=''>
-              <div className='flex text-primary font-bold'>
-                <div className='pr-2 '>
-                  <svg className='accordion-arrow rotate-90 h-full transition-all' xmlns="http://www.w3.org/2000/svg" width="7" height="11" viewBox="0 0 7 11" fill="none">
-                    <path d="M0 11L-4.80825e-07 0L7 5.5L0 11Z" fill="#024751"/>
-                  </svg>
-                </div>
-                <p>Общая площадь</p>
-                <span className='block flex-1 text-right'>12 345 кв.м</span>
-              </div>
-              <ul className='accordion-item border-t-2 border-[#E6EDEE] mt-2 pt-1'>
+              <ul className='hidden accordion-item border-t-2 border-[#E6EDEE] mt-2 pt-1'>
                 <li className='flex text-primary gap-1 mt-1'>
                   <p className='pl-[15px]'>Деревья</p>
                   <div className='basis-full h-[17px] border-b-[2px] border-[#ABC2C5] border-dotted'></div>

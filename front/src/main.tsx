67c5bdf06c8a3a1563bf13905debbc8fce82594a
Den Shi <iamdennshi@gmail.com>
2024-01-23 03:12:09 +0500

    Refactoring

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./components/App";
 import "./index.css";
 import { YMaps } from "@pbe/react-yandex-maps";
 
